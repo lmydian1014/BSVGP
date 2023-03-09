eb12d12fac2510b7e5cbd5b6da18594e5a93b5ac
Moyan Li <54998807+lmydian1014@users.noreply.github.com>
2023-03-09 15:46:59 -0500

    Update init.c

--- src/init.c
+++ src/init.c
@@ -8,38 +8,37 @@
 */
 
 /* .Call calls */
-extern SEXP _BSVGP_fun_dev(SEXP, SEXP);
-extern SEXP _BSVGP_fun_mul(SEXP, SEXP);
-extern SEXP _BSVGP_G_thres_cpp(SEXP, SEXP);
-extern SEXP _BSVGP_loglikelihood_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP _BSVGP_post_e_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP _BSVGP_post_tau_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP _BSVGP_rcpparma_bothproducts(SEXP);
-extern SEXP _BSVGP_rcpparma_hello_world();
-extern SEXP _BSVGP_rcpparma_innerproduct(SEXP);
-extern SEXP _BSVGP_rcpparma_outerproduct(SEXP);
-extern SEXP _BSVGP_sample_c_l_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP _BSVGP_sample_gibbs_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP _BSVGP_sample_thres_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _TCGP_fun_dev(SEXP, SEXP);
+extern SEXP _TCGP_fun_mul(SEXP, SEXP);
+extern SEXP _TCGP_G_thres_cpp(SEXP, SEXP);
+extern SEXP _TCGP_loglikelihood_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _TCGP_post_e_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _TCGP_post_tau_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _TCGP_rcpparma_bothproducts(SEXP);
+extern SEXP _TCGP_rcpparma_hello_world();
+extern SEXP _TCGP_rcpparma_innerproduct(SEXP);
+extern SEXP _TCGP_rcpparma_outerproduct(SEXP);
+extern SEXP _TCGP_sample_c_l_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _TCGP_sample_gibbs_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _TCGP_sample_thres_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_BSVGP_fun_dev",               (DL_FUNC) &_BSVGP_fun_dev,                2},
-    {"_BSVGP_fun_mul",               (DL_FUNC) &_BSVGP_fun_mul,                2},
-    {"_BSVGP_G_thres_cpp",           (DL_FUNC) &_BSVGP_G_thres_cpp,            2},
-    {"_BSVGP_loglikelihood_cpp",     (DL_FUNC) &_BSVGP_loglikelihood_cpp,     10},
-    {"_BSVGP_post_e_cpp",            (DL_FUNC) &_BSVGP_post_e_cpp,            13},
-    {"_BSVGP_post_tau_cpp",          (DL_FUNC) &_BSVGP_post_tau_cpp,          12},
-    {"_BSVGP_rcpparma_bothproducts", (DL_FUNC) &_BSVGP_rcpparma_bothproducts,  1},
-    {"_BSVGP_rcpparma_hello_world",  (DL_FUNC) &_BSVGP_rcpparma_hello_world,   0},
-    {"_BSVGP_rcpparma_innerproduct", (DL_FUNC) &_BSVGP_rcpparma_innerproduct,  1},
-    {"_BSVGP_rcpparma_outerproduct", (DL_FUNC) &_BSVGP_rcpparma_outerproduct,  1},
-    {"_BSVGP_sample_c_l_cpp",        (DL_FUNC) &_BSVGP_sample_c_l_cpp,        14},
-    {"_BSVGP_sample_gibbs_cpp",      (DL_FUNC) &_BSVGP_sample_gibbs_cpp,      20},
-    {"_BSVGP_sample_thres_cpp",      (DL_FUNC) &_BSVGP_sample_thres_cpp,      12},
+    {"_TCGP_fun_dev",               (DL_FUNC) &_TCGP_fun_dev,                2},
+    {"_TCGP_fun_mul",               (DL_FUNC) &_TCGP_fun_mul,                2},
+    {"_TCGP_G_thres_cpp",           (DL_FUNC) &_TCGP_G_thres_cpp,            2},
+    {"_TCGP_loglikelihood_cpp",     (DL_FUNC) &_TCGP_loglikelihood_cpp,     10},
+    {"_TCGP_post_e_cpp",            (DL_FUNC) &_TCGP_post_e_cpp,            13},
+    {"_TCGP_post_tau_cpp",          (DL_FUNC) &_TCGP_post_tau_cpp,          12},
+    {"_TCGP_rcpparma_bothproducts", (DL_FUNC) &_TCGP_rcpparma_bothproducts,  1},
+    {"_TCGP_rcpparma_hello_world",  (DL_FUNC) &_TCGP_rcpparma_hello_world,   0},
+    {"_TCGP_rcpparma_innerproduct", (DL_FUNC) &_TCGP_rcpparma_innerproduct,  1},
+    {"_TCGP_rcpparma_outerproduct", (DL_FUNC) &_TCGP_rcpparma_outerproduct,  1},
+    {"_TCGP_sample_c_l_cpp",        (DL_FUNC) &_TCGP_sample_c_l_cpp,        14},
+    {"_TCGP_sample_gibbs_cpp",      (DL_FUNC) &_TCGP_sample_gibbs_cpp,      20},
+    {"_TCGP_sample_thres_cpp",      (DL_FUNC) &_TCGP_sample_thres_cpp,      12},
     {NULL, NULL, 0}
 };
-
-void R_init_BSVGP(DllInfo *dll)
+void R_init_TCGP(DllInfo *dll)
 {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
