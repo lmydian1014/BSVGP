fbc20c8df4229f8b5b43f63e580ab59ed34e523d
Moyan Li <54998807+lmydian1014@users.noreply.github.com>
2023-08-07 18:47:20 +0800

    Update init.c

--- src/init.c
+++ src/init.c
@@ -19,7 +19,7 @@ extern SEXP _TCGP_rcpparma_hello_world();
 extern SEXP _TCGP_rcpparma_innerproduct(SEXP);
 extern SEXP _TCGP_rcpparma_outerproduct(SEXP);
 extern SEXP _TCGP_sample_c_l_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP _TCGP_sample_gibbs_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _TCGP_TCGP_fit(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP _TCGP_sample_thres_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
@@ -34,7 +34,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_TCGP_rcpparma_innerproduct", (DL_FUNC) &_TCGP_rcpparma_innerproduct,  1},
     {"_TCGP_rcpparma_outerproduct", (DL_FUNC) &_TCGP_rcpparma_outerproduct,  1},
     {"_TCGP_sample_c_l_cpp",        (DL_FUNC) &_TCGP_sample_c_l_cpp,        14},
-    {"_TCGP_sample_gibbs_cpp",      (DL_FUNC) &_TCGP_sample_gibbs_cpp,      20},
+    {"_TCGP_TCGP_fit",      (DL_FUNC) &_TCGP_TCGP_fit,      20},
     {"_TCGP_sample_thres_cpp",      (DL_FUNC) &_TCGP_sample_thres_cpp,      12},
     {NULL, NULL, 0}
 };
